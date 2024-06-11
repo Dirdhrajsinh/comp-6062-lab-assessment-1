4436d667936887a1b8f4d6df0638670dfd5cd1e2
Dirdhrajsinh <d_chauhan195091@fanshaweonline.ca>
2024-06-11 19:13:24 -0400

    Comp 6062 Lab Assessment 1

---
+++ app.js
@@ -0,0 +1,8 @@
+const studentName = "Dirdhrajsinh Chauhan"; 
+const studentNumber = "1157288"; 
+
+console.log(`${studentName} - ${studentNumber}`);
+
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = `${studentName} - ${studentNumber}`;
+headerContent.classList.add('heading1');
