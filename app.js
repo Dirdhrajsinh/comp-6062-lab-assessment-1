d51f7af0a48bd3c991af12a95eb1fb963dafb496
Dirdhrajsinh <d_chauhan195091@fanshaweonline.ca>
2024-06-11 19:24:19 -0400

    Rempoving the old files

--- app.js
+++
@@ -1,8 +0,0 @@
-const studentName = "Dirdhrajsinh Chauhan"; 
-const studentNumber = "1157288"; 
-
-console.log(`${studentName} - ${studentNumber}`);
-
-const headerContent = document.querySelector('h1');
-headerContent.innerHTML = `${studentName} - ${studentNumber}`;
-headerContent.classList.add('heading1');
